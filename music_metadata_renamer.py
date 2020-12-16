12d74ca6f880efe10b0482a205b9e1f61e5d78e7
enes4949 <enes.dogan26@gmail.com>
2020-12-16 13:47:42 +0300

    add filetype support

--- music_metadata_renamer.py
+++ music_metadata_renamer.py
@@ -3,13 +3,15 @@
 """Rename music files as {artist}-{album}-{title}."""
 
 from os import listdir, rename
-from tinytag import TinyTag, TinyTagException
+from tinytag import TinyTag
+import filetype
 
 files = (file for file in listdir())
 
 for file in files:
     try:
         tag = TinyTag.get(file)
-        rename(file, f'{tag.artist} - {tag.album} - {tag.title}.flac')
+        ext = filetype.guess(file).extension
+        rename(file, f'{tag.artist} - {tag.album} - {tag.title}.{ext}')
     except Exception:
         pass
