9ae95c0b8e28a36e6a4cc2c752f4256816f93196
enes4949 <enes.dogan26@gmail.com>
2020-12-20 15:02:06 +0300

    dont overwrite and some improvements

--- music_metadata_renamer.py
+++ music_metadata_renamer.py
@@ -2,14 +2,26 @@
 
 """Rename music files as {artist}-{album}-{title}."""
 
-from os import listdir, rename
+from os import listdir
+from os.path import isdir
+from shutil import move
 from tinytag import TinyTag, TinyTagException
+import filetype
 
 files = (file for file in listdir())
 
-for file in files:
-    try:
-        tag = TinyTag.get(file)
-        rename(file, f'{tag.artist} - {tag.album} - {tag.title}.flac')
-    except Exception:
-        pass
+
+if __name__ == "__main__":
+    for file in files:
+        if isdir(file):
+            continue
+        try:
+            tag = TinyTag.get(file)
+            ext = filetype.guess(file).extension
+            move(
+                file, f'{tag.artist} - {tag.album} - {tag.title}.{ext}')
+        except FileNotFoundError:
+            move(
+                file, f'{tag.artist.replace("/","")} - {tag.album.replace("/","")} - {tag.title.replace("/","")}.{ext}')
+        except TinyTagException:
+            pass
